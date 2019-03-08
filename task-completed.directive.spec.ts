ce98bb42dfc3dbfd6ae52dc94a6d650051b45f62
Maithri SG Krishnan <46931720+maithrisgk@users.noreply.github.com>
2019-03-08 14:14:52 -0500

    Delete task-completed.directive.spec.ts

--- task-completed.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { TaskCompletedDirective } from './task-completed.directive';
-
-describe('TaskCompletedDirective', () => {
-  it('should create an instance', () => {
-    const directive = new TaskCompletedDirective();
-    expect(directive).toBeTruthy();
-  });
-});
