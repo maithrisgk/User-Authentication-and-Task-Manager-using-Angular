446427fd3eaf42e9f7dbb2d35312573f74a552e9
Maithri SG Krishnan <46931720+maithrisgk@users.noreply.github.com>
2019-03-08 14:13:37 -0500

    Delete my-uppercase.pipe.spec.ts

--- my-uppercase.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { MyUppercasePipe } from './my-uppercase.pipe';
-
-describe('MyUppercasePipe', () => {
-  it('create an instance', () => {
-    const pipe = new MyUppercasePipe();
-    expect(pipe).toBeTruthy();
-  });
-});
